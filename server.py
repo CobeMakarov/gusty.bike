cc45190c1f4ec32feee4c6ca90dd88c5abd5317e
bwwalshjr <bwwalshjr@gmail.com>
2018-12-03 23:31:28 -0500

    Finished adding registration and linking it to admin.js and server.py, may need a few bugs worked out.

--- server.py
+++ server.py
@@ -129,9 +129,70 @@ def authenticate():
 
             session['user_id'] = user.create(email, password, username, db)
 
-            print(session['user_id'])
+            #print(session['user_id'])
 
-            session['authenticated'] = True
+            #session['authenticated'] = True
+
+            return '1'  # response 1 = SUCCESS!
+    else:
+        return None  # return nothing because the request isnt valid
+
+
+@app.route('/admin/users/authenticate', methods = ['GET', 'POST'])
+def register_user():
+    if request.method == "POST":
+
+        db.connect()
+
+        request_type = request.form["type"]
+
+        if request_type == "l":
+            if not request.form["username"] or not request.form["password"]:
+                return '2'  # response 2 = field blank
+
+            username = request.form["username"]
+            password = request.form["password"]
+
+            uid = user.login(username, password, db)
+
+            if uid == 0:
+                return '0'  # response 0 = fail
+            else:
+                session['user_id'] = uid
+                session['authenticated'] = True
+
+                return '1'  # response 1 = SUCCESS!
+
+        if request_type == "r":
+            if not request.form["username"] or not request.form["email"] or \
+                    not request.form["password"] or not request.form["password2"]:
+                return '2'  # response 2 = field blank
+
+            username = request.form["username"]
+            email = request.form["email"]
+            password = request.form["password"]
+            password2 = request.form["password2"]
+
+            if '@' not in email or '.' not in email:
+                return '3'  # response 3 = email not formatted correctly
+
+            if password != password2:
+                return '4'  # response 4 = passwords do not match
+
+            if not len(password) >= 6:
+                return '5'  # response 5 = password not long enough
+
+            if not username.isalnum() or len(username) >= 30:
+                return '6'  # response 6 = username does not fit criteria
+
+            if user.exists(username, db):
+                return '0'  # response 7 user already exists
+
+            session['user_id'] = user.create(email, password, username, db)
+
+            #print(session['user_id'])
+
+            #session['authenticated'] = True
 
             return '1'  # response 1 = SUCCESS!
     else:
--- static/js/admin.js
+++ static/js/admin.js
@@ -84,4 +84,39 @@ $(document).ready(function() {
             },
         });
     });
+
+    $('#completeRegistration').on('click', function() {
+        $.post('authenticate', {
+            'username': $("#registerUsername").val(),
+            'email': $("#registerEmail").val(),
+            'password': $("#registerPassword").val(),
+            'password2': $("#registerPassword2").val(),
+            'type': 'r'}, function(response) {
+            switch (response) {
+                case '0': //fail
+                    alertify.error("A user with that username already exists!");
+                    break;
+                case '1': //success
+                    //window.location.reload();
+                    alertify.success("User successfully created!");
+                    break;
+                case '2': //field blank
+                    alertify.error("You left something blank.");
+                    break;
+                case '3': //email not formatted correctly
+                    alertify.error("Your e-mail is not formatted correctly");
+                    break;
+                case '4': //passwords don't match
+                    alertify.error("Passwords do not match");
+                    break;
+                case '5': //password not long enough
+                    alertify.error("Passwords need to be a minimum of six characters.");
+                    break;
+                case '6': //username not formatted correctly
+                    alertify.error("Your username is not formatted correctly");
+                    break;
+            }
+            console.log(response);
+        })
+    });
 });
